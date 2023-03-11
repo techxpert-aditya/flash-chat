9eaed705ac1e42168a99814b28291041bf526825
aditya siddhpura <techxpert.aditya2409@gmail.com>
2023-03-11 15:37:07 +0530

    the animationcontroller is added, and the typewriter animaiton is added

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,28 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
 
+  const WelcomeScreen({super.key});
   @override
+  // ignore: library_private_types_in_public_api
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    AnimationController controller =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    controller.forward();
+    controller.addListener(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,13 +44,29 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                     ),
                   ),
                 ),
-                const Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 50.0,
                     fontWeight: FontWeight.w900,
+                    color: Color.fromARGB(221, 59, 11, 11),
+                    fontFamily: 'Agne',
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText('flash chat'),
+                      TypewriterAnimatedText('flash talks'),
+                      TypewriterAnimatedText('quick chats'),
+                    ],
+                    onTap: () {},
                   ),
                 ),
+                // const Text(
+                //   'Flash Chat',
+                //   style: TextStyle(
+                //     fontSize: 45.0,
+                //     fontWeight: FontWeight.w900,
+                //   ),
+                // ),
               ],
             ),
             const SizedBox(
